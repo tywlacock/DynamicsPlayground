5e712e3eeca0b19f35858aa1829549bfb66ed643
tylacock <tylacock@gmail.com>
2013-10-19 21:27:11 -0400

    Initial Commit

---
+++ DynamicsPlayground/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DynamicsPlayground
+//
+//  Created by Ty Lacock on 10/19/13.
+//  Copyright (c) 2013 Ty Lacock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DynamicsPlayground/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DynamicsPlayground
+//
+//  Created by Ty Lacock on 10/19/13.
+//  Copyright (c) 2013 Ty Lacock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
